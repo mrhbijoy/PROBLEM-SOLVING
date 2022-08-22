9fdeae5ea0079c3897ab2bda91d3e8e98383f96e
Hasan Bijoy <mrhbijoy@gmail.com>
2022-08-22 20:09:27 +0600

    new

---
+++ .vscode/beecrowd/1001.c
@@ -0,0 +1,10 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1001
+
+#include <stdio.h>
+int main() {
+    int a, b, x;
+    scanf("%d %d", &a, &b);
+    x = a + b;
+    printf("X = %d\n", x);
+    return 0;
+}
---
+++ error1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int;
+    //an example of compiler error
+    printf("Hello World!");
+    return 0;
+
+}
